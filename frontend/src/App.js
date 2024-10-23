0c2ea26fb923c9c9113d25da7bea1f1d85233f94
Manish Ratnawat <ratnawatmanish031@gmail.com>
2024-10-23 14:10:44 +0530

    some changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
     if (loggedInUser) {
   
-      const newSocket = io(process.env.REACT_APP_API_URL, {
+      const newSocket = io(`wss://chat-application-gxw2.onrender.com`, {
         transports: ['websocket', 'polling']
       }); // Backend URL
       setSocket(newSocket);
